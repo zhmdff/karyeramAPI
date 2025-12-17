7230f1061403a42608c05ccdf0645501d1f0379d
Mahmud Ahmadov <87231302+zhmdff@users.noreply.github.com>
2025-12-17 23:30:07 +0400

    ver

--- Models/User.cs
+++ Models/User.cs
@@ -3,12 +3,16 @@
     public class User
     {
         public int Id { get; set; }
-
-        public UserRole? UserRole { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public UserRole UserRole { get; set; } = UserRole.Guest;
         public bool IsVerified { get; set; } = false;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        
-        
+
+        public int? ProfileId { get; set; }
+        public EmployerProfile? EmployerProfile { get; set; }
+        public JobSeekerProfile? JobSeekerProfile { get; set; }
     }
 
     public enum UserRole
